e9f28eabda9af12331edeaeb2a7218071f9f126b
snwebdev <stewartnairn@live.com>
2020-05-23 00:12:16 +0100

    Delete timerOut.py

--- timerOut.py
+++
@@ -1,8 +0,0 @@
-import math
-
-
-def timerOut(t):
-    hours = math.floor(t / 60 / 60)
-    minutes = math.floor(t / 60 - hours * 60)
-    seconds = t - minutes * 60 - hours * 60 * 60
-    return ("{:0>2}:{:0>2}:{:0>2}".format(hours, minutes, seconds))
